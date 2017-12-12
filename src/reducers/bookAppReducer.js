d12d8586b3b434b8f29f675314f3a82a086ad534
Malind Kowalski <malind.nikulski@gmail.com>
2017-12-12 14:17:41 -0700

    modify with combineReducers()

--- src/reducers/bookAppReducer.js
+++ src/reducers/bookAppReducer.js
@@ -1,39 +1,36 @@
-export function bookApp(state = {
-  authors: [],
-  books: []
-}, action) {
+import {combineReducers} from 'redux'
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+})
+
+export const store = createStore(rootReducer)
+
+function authorsReducer(state = [], action) {
   switch(action.type) {
-    case 'ADD_BOOK':
-      return Object,assign(state, {
-        books: state.books.concat(action.book)
-      })
-
-    case 'REMOVE_BOOK':
-      const index = state.books.indexOf(action.id)
-      return Object.assign(state, {
-        books: [
-          state.books.slice(0, index),
-          state.books.slice(index + 1)
-        ]
-      })
-
     case 'ADD_AUTHOR':
-      return Object.assign(state, {
-        authors: state.authors.concat(action.author)
-      })
+      return state.concat(action.author)
 
     case 'REMOVE_AUTHOR':
-      const index = state.authors.indexOf(action.id)
-      return Object.assign(state, {
-        authors: [
-          state.authors.slice(0, index),
-          state.authors.slice(index + 1)
-        ]
-      })
+      const index = state.indexOf(action.id)
+      return [...state.slice(0, index), ...state.slice(index + 1)]
 
     default:
       return state
   }
 }
 
-export const store = createStore(bookApp)
+function booksReducer(state = [], action) {
+  switch(action.type) {
+    case 'ADD_BOOK':
+      return state.concat(action.book)
+
+    case 'REMOVE_BOOK':
+      const index = state.indexOf(action.id)
+      return [...state.slice(0, index), ...state.slice(index + 1)]
+
+    default:
+      return state
+  }
+}
