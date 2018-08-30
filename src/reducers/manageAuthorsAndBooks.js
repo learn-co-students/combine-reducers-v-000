b3dbc884429e2601ed8b07152ada41eac304c231
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-30 21:21:15 +0000

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -33,6 +33,16 @@ function authorsReducer(state = [], action) {
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+
     default:
       return state;
   }
