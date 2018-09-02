ababd4a3d97a7abacd3e215de2a88cd1d619bcdb
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-09-02 15:35:29 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Authors from './components/Authors';
 import BookInput from './components/BookInput';
 import Books from './components/Books';
 
-
 export class App extends Component {
   render() {
     return (
