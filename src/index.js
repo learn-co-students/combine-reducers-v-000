d99464f9fc2c34538147148afabc3a45b3e2fc84
Joanne Lee <vernapii@gmail.com>
2018-09-13 10:42:58 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,44 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
+import { combineReducers } from 'redux'
 
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+})
+
+export default rootReducer
+
+function booksReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [...state, action.book];
+
+    case "REMOVE_BOOK":
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    default:
+      return state;
+  }
+}
+
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+
+    case "REMOVE_AUTHOR":
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    default:
+      return state;
+  }
+}
 
 const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
