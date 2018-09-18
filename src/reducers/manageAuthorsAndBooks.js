6d5a54c4f2f275fd4cab319a7ccebe5f5eab76fa
Maverick Tesini <mavericktesini@gmail.com>
2018-09-18 01:16:55 +0000

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,43 +1,58 @@
-export default function bookApp(state = {
-  authors: [],
-  books: []
-}, action) {
-  let idx
-  switch (action.type) {
+import { combineReducers } from "redux";
 
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
+
+export default rootReducer;
+
+function booksReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
     case "ADD_BOOK":
-      return {
-        ...state,
-        books: [...state.books, action.book]
-      };
+      return [...state, action.book];
 
     case "REMOVE_BOOK":
-      idx = state.books.indexOf(action.id);
-      return {
-        ...state,
-        books: [
-          state.books.slice(0, idx),
-          state.books.slice(idx + 1),
-        ]
-      };
-
-    case "ADD_AUTHOR":
-        return {
-          ...state,
-          authors: [...state.authors, action.author]
-        };
-
-    case "REMOVE_AUTHOR":
-      idx = state.authors.indexOf(action.id);
-      return {
-        ...state,
-        authors: [
-          state.authors.slice(0, idx),
-          state.authors.slice(idx + 1)
-        ]
-      };
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
     default:
       return state;
-    }
-};
+  }
+}
+
+import uuid from "uuid";
+
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+
+    case "REMOVE_AUTHOR":
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+
+    default:
+      return state;
+  }
+}
+
+
+// For learning purposes, our two reducers are in the same file,
+// but it is common to separate each reducer into its own file.
+// You could then either import each reducer into a new file,
+// something like reducers/rootReducer.js,
+//  where combineReducer is called. Or, alternatively,
+//  you could include combineReducer in your src/index.js file.
