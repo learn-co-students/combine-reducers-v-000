5d5e699f0761594c9ba88c9fa67bd72a8474bcc4
github username <github email address>
2018-12-11 12:26:26 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Authors from './components/Authors';
 import BookInput from './components/BookInput';
 import Books from './components/Books';
 
-
+//
 export class App extends Component {
   render() {
     return (
