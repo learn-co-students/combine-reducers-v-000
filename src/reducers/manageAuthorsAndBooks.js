aa3723b627cdfa4f6661da16764e00233bdc3f22
Obafemi Fadairo <obafad94@gmail.com>
2019-01-13 20:52:34 -0500

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,41 +1,53 @@
-export default function bookApp(state = {
-  authors: [],
-  books: []
-}, action) {
-  let idx
+import {combineReducers} from "redux";
+import uuid from "uuid";
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
+
+export default rootReducer;
+
+function booksReducers(state = [], action) {
+  let idx;
   switch (action.type) {
 
     case "ADD_BOOK":
-      return {
-        ...state,
-        books: [...state.books, action.book]
-      };
+      return [
+        ...state, action.book]
 
     case "REMOVE_BOOK":
       idx = state.books.indexOf(action.id);
-      return {
-        ...state,
-        books: [
-          state.books.slice(0, idx),
-          state.books.slice(idx + 1),
-        ]
-      };
+      return [
+        ...state.slice(0, idx),
+        ...state.books.slice(idx + 1)]
 
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(author +> author.authorNmae === action.book.authorName);
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [
+        ...state, {authorName: action.book.authorName, id: uuid()}];
+      }
+
+      default:
+        return state;
+      }
+    }
+
+function authorsReducers(state = [], action) {
+  let idx;
+  switch(action.type) {
     case "ADD_AUTHOR":
-        return {
-          ...state,
-          authors: [...state.authors, action.author]
-        };
+        return [...state, action.author]
 
     case "REMOVE_AUTHOR":
       idx = state.authors.indexOf(action.id);
-      return {
-        ...state,
-        authors: [
-          state.authors.slice(0, idx),
+      return [
+        ...state.slice(0, idx),
           state.authors.slice(idx + 1)
         ]
-      };
 
     default:
       return state;
