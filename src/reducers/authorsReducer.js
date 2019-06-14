a5249c6db2f36969f072716ff0fb4decf09dc472
David McGrann <dpmcgrann@gmail.com>
2019-06-14 16:03:01 -0400

    Done.

---
+++ src/reducers/authorsReducer.js
@@ -0,0 +1,28 @@
+import uuid from "uuid";
+
+const authorsReducer = (state = [], action) => {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+
+    case "REMOVE_AUTHOR":
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+
+    default:
+      return state;
+  }
+}
+
+export default authorsReducer
