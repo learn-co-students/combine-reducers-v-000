718f91f87651fcd7394154f974673cf309de5822
NotoriousCottonball <gpianka1@gmail.com>
2019-07-10 00:41:38 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+/*
+You could then either import each reducer into a new file, something like reducers/rootReducer.js, where combineReducer is called. Or, alternatively, you could include combineReducer in your src/index.js file. For example:
+*/
+import { combineReducers } from "redux";
+import authorsReducer from './reducers/authorsReducer';
+import booksReducer from './reducers/booksReducer';
+/*
+import rootReducer from './reducers/manageAuthorsAndBooks';
+*/
+
+/*
 import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
 
-
 const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+*/
+
+const rootReducer = combineReducers({
+  books: booksReducer,
+  authors: authorsReducer
+})
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,3 +1,60 @@
+import { combineReducers } from "redux";
+import uuid from "uuid";
+/*
+Note: We're using a useful package, uuid, to handle unique ID generation. With this refactor, since we are creating an author ID from within the reducer instead of in AuthorInput.js, we need to import it here as well.
+*/
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
+
+export default rootReducer;
+
+function booksReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [...state, action.book];
+
+    case "REMOVE_BOOK":
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    default:
+      return state;
+  }
+}
+
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+
+    case "REMOVE_AUTHOR":
+      idx = state.indexOf(action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+
+    default:
+      return state;
+  }
+}
+
+
+
+
+/*
 export default function bookApp(state = {
   authors: [],
   books: []
@@ -41,3 +98,4 @@ export default function bookApp(state = {
       return state;
     }
 };
+*/
