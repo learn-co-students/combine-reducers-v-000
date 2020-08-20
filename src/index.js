2b5a788a7a0ca8c5c1d410c13f27123b68fa9330
sdcro <sdcrouse91@gmail.com>
2020-08-19 19:19:41 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
+import rootReducer from './reducers/manageAuthorsAndBooks';
+// import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
+// I thought that it was a mistake to use a different name than bookApp (the name of the exported function from the reducer).
+// However, that is perfectly valid code, as it is aliasing the name of the function.
+// See https://github.com/learn-co-curriculum/react-modular-code
 
+// const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,39 +1,96 @@
-export default function bookApp(
-  state = {
-    authors: [],
-    books: []
-  },
-  action
-) {
+import { combineReducers } from "redux";
+import uuid from "uuid";
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
+
+export default rootReducer;
+
+function booksReducer(state = [], action) {
   let idx;
   switch (action.type) {
     case "ADD_BOOK":
-      return {
-        ...state,
-        books: [...state.books, action.book]
-      };
+      return [...state, action.book];
 
     case "REMOVE_BOOK":
-      idx = state.books.findIndex(book => book.id === action.id);
-      return {
-        ...state,
-        books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
-      };
-
-    case "ADD_AUTHOR":
-      return {
-        ...state,
-        authors: [...state.authors, action.author]
-      };
-
-    case "REMOVE_AUTHOR":
-      idx = state.authors.findIndex(author => author.id === action.id);
-      return {
-        ...state,
-        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
-      };
+      idx = state.findIndex(book => book.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
     default:
       return state;
   }
 }
+
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+
+    case "REMOVE_AUTHOR":
+      idx = state.findIndex(author => author.id === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    case "ADD_BOOK":
+      // WHERE do action.book and action.author get set?!
+      // Answer: in actions/index.js (remember ES6 object syntax)
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+    
+    default:
+      return state;
+  }
+}
+
+// Default code:
+// export default function bookApp(
+//   state = {
+//     authors: [],
+//     books: []
+//   },
+//   action
+// ) {
+//   let idx;
+//   switch (action.type) {
+//     case "ADD_BOOK":
+//       return {
+//         ...state,
+//         authors: [...state.authors], // This is in the README, but is it even needed? Update: Yes it is, in order to have immutable state!
+//         books: [...state.books, action.book]
+//       };
+
+//     case "REMOVE_BOOK":
+//       idx = state.books.findIndex(book => book.id === action.id);
+//       return {
+//         ...state,
+//         authors: [...state.authors],
+//         books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
+//       };
+
+//     case "ADD_AUTHOR":
+//       return {
+//         ...state,
+//         books: [...state.books],
+//         authors: [...state.authors, action.author]
+//       };
+
+//     case "REMOVE_AUTHOR":
+//       idx = state.authors.findIndex(author => author.id === action.id);
+//       return {
+//         ...state,
+//         books: [...state.books],
+//         authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
