d109bfb0972925feb2e0474d9566225da697596a
yurigurlie207 <adrienne.yao@gmail.com>
2021-03-28 15:15:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
-
+ /* nothing needs to change unless we want manageAuthrosAndBooks to just be called rootReducer instead */
 
 const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
@@ -14,3 +14,16 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+
+//if reducers are in separate files
+// import authorsReducer from './reducers/authorsReducer';
+// import booksReducer from './reducers/booksReducer';
+ 
+// const rootReducer = combineReducers({
+//   books: booksReducer,
+//   authors: authorsReducer
+// })
+ 
+// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+ 
