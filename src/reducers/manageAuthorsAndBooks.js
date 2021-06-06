8ee954f53c34b73341fa328c2c5b8db92d588aef
github username <github email address>
2021-06-06 08:51:39 -0600

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,38 +1,47 @@
-export default function bookApp(
-  state = {
-    authors: [],
-    books: []
-  },
-  action
-) {
+import { combineReducers } from "redux";
+ 
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
+ 
+export default rootReducer;
+ 
+function booksReducer(state = [], action) {
   let idx;
   switch (action.type) {
     case "ADD_BOOK":
-      return {
-        ...state,
-        books: [...state.books, action.book]
-      };
-
+      return [...state, action.book];
+ 
     case "REMOVE_BOOK":
-      idx = state.books.findIndex(book => book.id === action.id);
-      return {
-        ...state,
-        books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
-      };
-
+      idx = state.findIndex(book => book.id  === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+ 
+    default:
+      return state;
+  }
+}
+ 
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
     case "ADD_AUTHOR":
-      return {
-        ...state,
-        authors: [...state.authors, action.author]
-      };
-
+      return [...state, action.author];
+ 
     case "REMOVE_AUTHOR":
-      idx = state.authors.findIndex(author => author.id === action.id);
-      return {
-        ...state,
-        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
-      };
+      idx = state.findIndex(author => author.id  === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
 
+    case "ADD_BOOK":
+      let existingAuthor = state.filter(
+        author => author.authorName === action.book.authorName
+      );
+      if (existingAuthor.length > 0) {
+        return state;
+      } else {
+        return [...state, { authorName: action.book.authorName, id: uuid() }];
+      }
+ 
     default:
       return state;
   }
