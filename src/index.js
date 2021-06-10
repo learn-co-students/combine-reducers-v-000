3b8bfe6471656cea029b8d85031557c4e0e9c402
parisdolfman <parisdolfman@gmail.com>
2021-06-10 14:51:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import manageAuthorsAndBooks from './reducers/manageAuthorsAndBooks';
-
-
-const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App store={store}/>
-  </Provider>,
-  document.getElementById('root')
-);
+import { combineReducers } from "redux";
+ 
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
+ 
+export default rootReducer;
+ 
+function booksReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [...state, action.book];
+ 
+    case "REMOVE_BOOK":
+      idx = state.findIndex(book => book.id  === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+ 
+    default:
+      return state;
+  }
+}
+ 
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_AUTHOR":
+      return [...state, action.author];
+ 
+    case "REMOVE_AUTHOR":
+      idx = state.findIndex(author => author.id  === action.id)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+ 
+    default:
+      return state;
+  }
+}
