87cbd92ecefb72be427a5d3cf17c840bc65e9996
emanao <emanao@me.com>
2021-06-14 15:34:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && wi
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
